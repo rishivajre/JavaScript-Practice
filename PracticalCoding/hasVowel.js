ce4b959c0b25a1d682f5984aa2e7b1a49dba0c18
rishivajre <rishikeshvajre7@gmail.com>
2025-04-03 09:43:18 +0530

    Add vowel checking functions with multiple approaches in hasVowel.js

---
+++ PracticalCoding/hasVowel.js
@@ -0,0 +1,42 @@
+// Approach 01: Using a loop and character checking
+function hasVowel(str) {
+    str = str.toLowerCase();
+    for (let i=0; i<str.length; i++) {
+        const char = str[i];
+        if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+            return true; // Vowel found
+        }
+    }
+    return false; // No vowel found
+}
+
+// Example usage:
+console.log(hasVowel("hello")); 
+console.log('-------------------');
+
+//Approach 02: Using REGEX
+function hasVowelRegex(str) {
+    return /[aeiou]/i.test(str); // Check for vowels using regex
+}
+
+// Example usage:
+console.log(hasVowelRegex("hello")); // true
+console.log(hasVowelRegex("sky")); // false
+console.log('-------------------'); 
+
+//Approach 03: Using Array and some() method
+function hasVowelArray(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    return str.toLowerCase().split('').
+        some(char => vowels.includes(char)); // Check if any character is a vowel
+}
+console.log(hasVowelArray("Hellos"));
+console.log('-------------------');
+
+//Aproach 04: Using Set Intersection
+function hasVowelSet(str) {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u']); // Creates a set of vowels
+    return [...str.toLowerCase()].some(char => vowels.has(char)); 
+    // Checks if any character is a vowel
+}
+console.log(hasVowelSet("Hellos"));
