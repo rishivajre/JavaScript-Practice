5c5f0424404333220889b82272fc20cc93f8b440
Rishikesh Vajre <rishikeshvajre7@gmail.com>
2025-07-16 21:45:31 +0530

    Add Variables and Data Types example in Variables&DataTypes.js

---
+++ REVISION/BASICS/Variables&DataTypes.js
@@ -0,0 +1,24 @@
+console.log("Hello World");
+// This is a simple JavaScript program that prints "Hello World" to the console.
+
+//✅ Step 2: Variables (var, let, const)
+var oldway = "var variable"; //Old way of declaring variables
+let modernWay = "let variable"; //Modern way of declaring variables
+const constantValue = "const variable"; // Cannot be changed later
+
+// ✅ Step 3: Data Types
+let str = "hell"; // string
+let num = 44;   // number
+let bool = "true"; // boolean
+let nothing = null; // null
+let notDefined; // undefined
+let obj = {name: "Ram", age: 33}; // object created for a person as a key-value pair
+let arr = [1,2,3,4,55]; // array created for a list of numbers
+
+// 👉 Practice: Use "typeof" on each variable to confirm data type:
+console.log(typeof str); // string
+console.log(typeof num); //number
+console.log(typeof obj); //object
+console.log(typeof arr); //object (arrays are also objects in JS)
+console.log(typeof bool); //string (because "true" is in quotes
+
