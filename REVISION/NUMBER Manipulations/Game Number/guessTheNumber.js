7819665cae994dd6305130012ced75d2c7dcf70e
Rishikesh Vajre <rishikeshvajre7@gmail.com>
2025-07-20 22:39:15 +0530

    Add guessTheNumber.js for a number guessing game implementation

---
+++ REVISION/NUMBER Manipulations/Game Number/guessTheNumber.js
@@ -0,0 +1,11 @@
+// Practice Qs
+const prompt = require('prompt-sync')();
+
+let gmaeNum = 25;
+
+let userNum = prompt("Guess the game Number: ");
+
+while (userNum != gmaeNum) {
+    userNum = prompt("Ooops So Close...Guess Again!: ");
+}
+console.log("Congrattsss!!!! You Hit It!");
