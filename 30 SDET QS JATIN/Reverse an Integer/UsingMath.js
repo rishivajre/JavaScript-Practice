a161cef8278bb53ee7b7e7c676e1dd894dda0847
rishikeshvajre <rishikeshvajre7@gmail.com>
2025-08-26 07:18:49 +0530

    Refactor reverseInteger function: improve formatting and update example input

--- 30 SDET QS JATIN/Reverse an Integer/UsingMath.js
+++ 30 SDET QS JATIN/Reverse an Integer/UsingMath.js
@@ -1,12 +1,13 @@
+// Using Math and While Loop
 function reverseInteger(num) {
   let result = 0;
   const sign = num < 0 ? -1 : 1;
   num = Math.abs(num);
 
   while (num !== 0) {
-    if (result > Number.MAX_SAFE_INTEGER / 10) return 0;
+    
+    if (result > Number.MAX_SAFE_INTEGER/10) return 0;
     let lastDigit = num % 10;
-
     result = result * 10 + lastDigit;
 
     num = Math.floor(num / 10);
@@ -15,4 +16,4 @@ function reverseInteger(num) {
   return result * sign;
 }
 
-console.log(reverseInteger(12345));
+console.log(reverseInteger(12344));
