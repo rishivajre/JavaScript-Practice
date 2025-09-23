9bbde7cac8d513880368e894e62b9adbdf349e40
rishikeshvajre <rishikeshvajre7@gmail.com>
2025-09-23 10:52:12 +0530

    PW-CY Interview Qs Folder Added

---
+++ Interview Quesitons/CountCharFrequencies.js
@@ -0,0 +1,19 @@
+function findDuplicateLetters(str) {
+  const freqMap = {};
+  const normalizedStr = str.toLowerCase().replace(/[^a-z]/g, ""); //Only Letters
+
+  //count Frequency of each letter
+  for (const char of normalizedStr) {
+    freqMap[char] = (freqMap[char] || 0) + 1;
+  }
+
+  //print duplicates
+  console.log("Duplicate letters and their frequencies");
+  for (const [char, count] of Object.entries(freqMap)) {
+    if (count > 1) {
+        console.log(`${char} => ${count}`);
+    }
+  }
+}
+
+findDuplicateLetters("Hello, Geordon")
