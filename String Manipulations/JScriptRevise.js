9dda86c804d1b1ce7fd9680bd08d52b192c187f8
rishikeshvajre <rishikeshvajre7@gmail.com>
2025-10-29 00:05:39 +0530

    More Revisions

--- String Manipulations/JScriptRevise.js
+++ String Manipulations/JScriptRevise.js
@@ -39,4 +39,57 @@ console.log("Expression "+ sub(1000,5)); // Function: Expression
 
 const prod = (a, b) => a * b; 
 console.log("Arrow Func "+ prod(2,2)); // Arrow Function
-console.log('---------------------------------------');
+console.log('-------------Object Functions----------------------');
+
+// Object Functions : Array of Keys & Values retrieval
+const user = {
+    name: 'Rishi',
+    age: 29,
+    ID: 1099
+};
+const keys = Object.keys(user);
+const val = Object.values(user);
+const userObjArr = Object.entries(user);
+console.log("Keys: "+keys+" & "+"Values: "+val+"\n");
+console.log("Entries: "+userObjArr);
+console.log('----------Object.assign(target, source)-------------------------');
+
+const src = {
+    a: 1,
+    b: 2
+};
+const trgt = {
+    c: 3
+};
+Object.assign(trgt, src);
+console.log(trgt); 
+Object.assign(src, trgt);
+console.log(src);
+
+console.log('-------------------------Callbacks------------------------------');
+/**A callback is a function passed as an argument to another function, 
+ * which is then invoked inside the outer function to complete some routine or action.
+ * Callbacks are foundational for handling asynchronous operations, but can also be used in synchronous code. */
+
+function greet(name, cb) {
+    cb("Hello " + name);
+}
+greet("Rishi", msg => console.log(msg));
+
+console.log('------------Synchronous Callback Example (Array methods)---------------');
+
+const num = [1,2,3];
+num.forEach(num => {
+    console.log(num * 2);
+})
+
+console.log('------------Asynchronous Callback Example (setTimeout)-----------');
+
+console.log('start');
+
+function finish() {
+    console.log('finish');
+}
+
+setTimeout(finish, 2000); // The 'finish' function is the callback.
+console.log("waiting...");
